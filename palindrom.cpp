2db682c25705514c2cb4dc6482267498d0059ee3
Md Sajibur Rahman <83011864+GameDevSajib@users.noreply.github.com>
2023-02-03 09:31:08 +0000

    Update palindrom.cpp

--- palindrom.cpp
+++ palindrom.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        bool result = true;
 
+        string s=to_string(x);
+        int n = s.size();
+
+        for(int i = 0; i < n/2; i++) {
+            if(s[i] != s[n-1-i]) {
+                result = false;
+                break;
+            }
+        } 
+        
+        // Memory = O(1)
+        // time = O(n)
+        
+        return result;
+    }
+};
